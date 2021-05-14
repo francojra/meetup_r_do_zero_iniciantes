a5a9dca866e9a1aa707c57fbe3d6f54bbbe122f1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-05-14 18:46:16 -0300

    Parte 3

---
+++ baixando_pacotes_3.R
@@ -0,0 +1,37 @@
+# R-Ladies Natal - Meetup
+# R do zero
+# Ministrante: Jeanne Franco
+# Data: 21/05/21
+
+# Pacotes -------------------------------------------------------------------------------------
+
+# Install.packages("nome do pacote")
+# Tools > Install.packages
+# Em "Packages" é possível visualizar todos os pacotes disponíveis 
+# com uma pequena descrição sobre cada um deles.
+
+# Acesse "https://cran.r-project.org/" clique em "Packages" > 
+# "CRAN Task Views" para ter a opção 
+# dos pacotes disponíveis por tópicos de diferentes análises estatísticas.
+
+
+# Função library para carregar o pacote --------------------------------------------------------------------------------
+
+library(ggplot2) # Verifica a instalação do pacote
+library(help = "ggplot2") # Abre um documento com informações 
+# sobre o pacote
+
+# Instalação de pacotes
+# Exemplos
+
+install.packages("car")
+install.packages("lme4")
+install.packages("nlme")
+install.packages("RColorBrewer")
+install.packages("tree")
+
+
+
+
+
+
